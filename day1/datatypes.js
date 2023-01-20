c76a5507366c77185148441b09b2d6d7bfb73148
gugasant <gustavosantlopes3@gmail.com>
2023-01-20 20:36:53 -0300

    day1

---
+++ day1/datatypes.js
@@ -0,0 +1,5 @@
+console.log(typeof 'Gustavo');
+console.log(typeof 77);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ day1/variables.js
@@ -0,0 +1,18 @@
+let umaString = 'Isso é uma string';
+let umNumber = 30;
+let genderMale = true;
+let indefinida
+let nulo = null;
+
+let firstName = 'Gustavo';
+let lastName = 'Santana';
+let isMarried = false;
+let country = 'Brazil';
+let age = 30
+
+let primeiroNome = 'Gustavo', segundoNome = 'Santana', estaCasado = false, paisOrigem = 'Brasil', idade = 30;
+
+let myAge = 30;
+let yourAge = 25;
+console.log('I am', myAge, 'years old.');
+console.log('You are', yourAge, 'years old.');
